047d18fa5e60b5bbce78c45f0fe24740897b7ba4
Michał Wolny <mwolny98@interia.pl>
2023-07-17 15:15:54 +0200

    changes

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,13 +1,142 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-import express, { Router } from 'express';
-import serverless from 'serverless-http';
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const querystring = require("querystring");
+const axios = require("axios");
 
 const api = express();
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+const stateKey = "spotify_auth_state";
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 const router = Router();
-router.get('/hello', (req, res) => res.send('Hello World!'));
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use('/api/', router);
+router.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
 
+  const scope =
+    "user-read-private user-read-email user-top-read user-read-currently-playing playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private";
+
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+router.get("/callback", (req, res) => {
+  const code = req.query.code || null;
+
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: REDIRECT_URI,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        const { access_token, refresh_token, expires_in } = response.data;
+
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+          expires_in,
+        });
+
+        res.redirect(`http://localhost:5173/dashboard?${queryParams}`);
+      } else {
+        res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+router.get("/refresh_token", (req, res) => {
+  const { refresh_token } = req.query;
+  console.log("wbilem na refresh");
+
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        const { access_token, refresh_token, expires_in } = response.data;
+
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+          expires_in,
+        });
+
+        res.redirect(`http://localhost:5173/dashboard?${queryParams}`);
+      } else {
+        res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+api.use("/api/", router);
+
+api.use(bodyParser.json());
+api.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+api.listen(8080);
 export const handler = serverless(api);
